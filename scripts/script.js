39988fcbc7e279e7331399cde1ae334bd3fd3361
kamp <lexiaky@gmail.com>
2025-12-16 13:41:10 -0500

    part 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
-import './styles/style.css'
+//import '../styles/style.css'
+
+const mains = document.getElementsByTagName("main");
+const mainEl = mains[0]
+console.log(`This is my mainEl: ${mainEl}`)
+
+const root = document.documentElement; 
+const styles = window.getComputedStyle(root);
+const color = styles.getPropertyValue('--main-bg')
+
+
+mainEl.style.backgroundColor = color
+let h1 = document.createElement('h1')
+h1.textContent = "DOM Manipulation"
+mainEl.append(h1)
+mainEl.classList.add("flex-ctr")
