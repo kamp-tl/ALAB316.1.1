709c81b29e0b4a3f1950a9267a96ed88b673194d
kamp <lexiaky@gmail.com>
2025-12-16 17:46:13 -0500

    var()

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,8 @@ const menuLinks = [
     { text: 'account', href: '/account' },
   ];
 
+
+//const mainEl = document.querySelector('main')  
 const mains = document.getElementsByTagName("main");
 const mainEl = mains[0]
 
@@ -14,6 +16,7 @@ const root = document.documentElement;
 const CSSstyles = window.getComputedStyle(root);
 
 mainEl.style.backgroundColor = CSSstyles.getPropertyValue('--main-bg')
+// mainEl.style.backgroundColor = 'var(--main-bg)'
 let h1 = document.createElement('h1')
 h1.textContent = "DOM Manipulation"
 mainEl.append(h1)
@@ -22,6 +25,7 @@ mainEl.classList.add("flex-ctr")
 const topMenuEl = document.getElementById("top-menu")
 topMenuEl.style.height = "100%"
 topMenuEl.style.backgroundColor = CSSstyles.getPropertyValue('--top-menu-bg')
+// topMenuEl.style.backgroundColor = 'var(--top-menu-bg)'
 topMenuEl.classList.add("flex-around")
 
 for (let link of menuLinks){
