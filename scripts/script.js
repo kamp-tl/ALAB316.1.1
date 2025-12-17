45739fd5b7bba1662ed12a60afe0571f428736d6
kamp <lexiaky@gmail.com>
2025-12-17 17:24:11 -0500

    part 4

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,22 @@
 //import '../styles/style.css'
 
 const menuLinks = [
-    { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
-  ];
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
 
 
 //const mainEl = document.querySelector('main')  
@@ -40,5 +51,19 @@ const subMenuEl = document.getElementById("sub-menu")
 subMenuEl.style.height = '100%'
 subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)'
 subMenuEl.classList.add('flex-around')
-subMenuEl.style.position = absolute
+subMenuEl.style.position = 'absolute'
 subMenuEl.style.top = 0
+
+//interation 
+const topMenuLinks = topMenuEl.querySelectorAll('a')
+topMenuEl.addEventListener('click', (e) =>{
+e.preventDefault()
+console.log(e.target.textContent)
+
+for(let a of topMenuLinks){
+  a.classList.remove('active')
+  if (a===e.target){continue}
+}
+e.target.classList.toggle('active')
+})
+
